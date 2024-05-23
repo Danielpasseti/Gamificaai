c912abf7045f591193fea64ec4955232f505b7a4
DANIEL DE FREITAS PASSETI PATRICIO <39969184881@info.local>
2024-05-23 10:24:34 -0300

    feat: Carrossel feito

--- script.js
+++ script.js
@@ -2,49 +2,100 @@ let menu = document.getElementById("menu")
 let iconeBarras = document.getElementById("icone-barras")
 let iconeX = document.getElementById("icone-x")
 
+function abreFechaMenu() {    
+    // Menu fechado - tem a classe menu-fechado
+    // Menu aberto - não tem a classe menu-fechado
 
+    // Alterna a classe "menu-fechado" no menu
+    // menu.classList.toggle("menu-fechado")
 
-function abrefechaMenu(){
-   if(menu.classList.contains("menu-fechado")){
-    menu.classList.remove("menu-fechado")
-    iconeX.style.display = "inline"
-    iconeBarras.style.display = "none"
+    // Se o menu contem a classe menu-fechado
+    if ( menu.classList.contains("menu-fechado") ) {
+        // Abrir o menu - remover a classe menu-fechado
+        menu.classList.remove("menu-fechado")
 
+        // Esconder icone barras
+        iconeBarras.style.display = "none"
 
+        // Mostrar o icone do X
+        iconeX.style.display = "inline"
 
-   }
-   else{
+    } else {
+        // Fechar o menu - adicionar a classe menu-fechado
+        menu.classList.add("menu-fechado")
 
-    menu.classList.add("menu-fechado")
-  
-    iconeBarras.style.display = "inline"
-    iconeX.style.display = "none"
+        // Esconder icone do X
+        iconeX.style.display = "none"
 
-   }
+        // Mostrar o icone barras
+        iconeBarras.style.display = "inline"
+
+    }
 }
 
 onresize = () => {
+    // Abrir o menu - remover a classe menu-fechado
     menu.classList.remove("menu-fechado")
-    iconeX.style.display = "inline"
+
+    // Esconder icone barras
     iconeBarras.style.display = "none"
+
+    // Mostrar o icone do X
+    iconeX.style.display = "inline"
 }
 
+// Carrossel
 let banner = document.querySelector(".banner")
 
-let slide = [
+// let slides = [0, 1, 2]
+// slides[0] -> primeiro-banner
+// slides[1] -> segundo-banner
+// slides[2] -> terceiro-banner
+
+let slides = [
     "primeiro-banner",
     "segundo-banner",
     "terceiro-banner"
-
 ]
 
 let slideAtual = 0
 
-banner.classList.add(slide[slideAtual])
-function mostrarProximoSlide(){
-    banner.classList.remove(slide[slideAtual])
+banner.classList.add(slides[slideAtual])
 
-    slideAtual++
+function mostrarProximoSlide() {
+    // Remover o slide anterior
+    banner.classList.remove(slides[slideAtual])
 
-    banner
+    if(slideAtual < 2 ){
+        // Somar 1 na variavel slideAtual
+        slideAtual++   
+    }else{
+        slideAtual = 0
+    }
+
+
+
+    // Mostrar slide de acordo com o slide atual
+    banner.classList.add(slides[slideAtual])
 }
+
+function mostrarSlideAnterior(){
+    banner.classList.remove(slides[slideAtual])
+    if(slideAtual > 0 ){
+        // Somar 1 na variavel slideAtual
+        slideAtual--  
+    }else{
+        slideAtual = 2
+    }
+    
+
+    banner.classList.add(slides[slideAtual])
+
+}
+
+function selecionarSlide(indiceSlide){
+    slideAtual = indiceSlide
+    banner.classList.add(slides[slideAtual])
+
+}
+
